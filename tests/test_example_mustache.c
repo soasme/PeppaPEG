dd017d6b51884f6ac870a66ce56ea16c86a219fe
soasme <soasme@gmail.com>
2021-02-15 03:15:36 +0000

    test mustache section open/close.

--- tests/test_example_mustache.c
+++ tests/test_example_mustache.c
@@ -83,12 +83,96 @@ P4_PRIVATE(void) test_partial(void) {
     P4_DeleteGrammar(grammar);
 }
 
+P4_PRIVATE(void) test_section_open(void) {
+    P4_Grammar* grammar = P4_CreateMustacheGrammar();
+    TEST_ASSERT_NOT_NULL(grammar);
+
+    P4_Source* source;
+
+    source = P4_CreateSource("{{#xyz}}", P4_MustacheTag);
+    TEST_ASSERT_NOT_NULL(source);
+    TEST_ASSERT_EQUAL(
+        P4_Ok,
+        P4_Parse(grammar, source)
+    );
+    TEST_ASSERT_EQUAL(8, P4_GetSourcePosition(source));
+    P4_DeleteSource(source);
+
+    source = P4_CreateSource("{{# xyz }}", P4_MustacheTag);
+    TEST_ASSERT_NOT_NULL(source);
+    TEST_ASSERT_EQUAL(
+        P4_Ok,
+        P4_Parse(grammar, source)
+    );
+    TEST_ASSERT_EQUAL(10, P4_GetSourcePosition(source));
+    P4_DeleteSource(source);
+
+    P4_DeleteGrammar(grammar);
+}
+
+P4_PRIVATE(void) test_section_open_inverted(void) {
+    P4_Grammar* grammar = P4_CreateMustacheGrammar();
+    TEST_ASSERT_NOT_NULL(grammar);
+
+    P4_Source* source;
+
+    source = P4_CreateSource("{{^xyz}}", P4_MustacheTag);
+    TEST_ASSERT_NOT_NULL(source);
+    TEST_ASSERT_EQUAL(
+        P4_Ok,
+        P4_Parse(grammar, source)
+    );
+    TEST_ASSERT_EQUAL(8, P4_GetSourcePosition(source));
+    P4_DeleteSource(source);
+
+    source = P4_CreateSource("{{^ xyz }}", P4_MustacheTag);
+    TEST_ASSERT_NOT_NULL(source);
+    TEST_ASSERT_EQUAL(
+        P4_Ok,
+        P4_Parse(grammar, source)
+    );
+    TEST_ASSERT_EQUAL(10, P4_GetSourcePosition(source));
+    P4_DeleteSource(source);
+
+    P4_DeleteGrammar(grammar);
+}
+
+P4_PRIVATE(void) test_section_close(void) {
+    P4_Grammar* grammar = P4_CreateMustacheGrammar();
+    TEST_ASSERT_NOT_NULL(grammar);
+
+    P4_Source* source;
+
+    source = P4_CreateSource("{{/xyz}}", P4_MustacheTag);
+    TEST_ASSERT_NOT_NULL(source);
+    TEST_ASSERT_EQUAL(
+        P4_Ok,
+        P4_Parse(grammar, source)
+    );
+    TEST_ASSERT_EQUAL(8, P4_GetSourcePosition(source));
+    P4_DeleteSource(source);
+
+    source = P4_CreateSource("{{/ xyz }}", P4_MustacheTag);
+    TEST_ASSERT_NOT_NULL(source);
+    TEST_ASSERT_EQUAL(
+        P4_Ok,
+        P4_Parse(grammar, source)
+    );
+    TEST_ASSERT_EQUAL(10, P4_GetSourcePosition(source));
+    P4_DeleteSource(source);
+
+    P4_DeleteGrammar(grammar);
+}
+
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_whitespace);
     RUN_TEST(test_variable);
     RUN_TEST(test_partial);
+    RUN_TEST(test_section_open);
+    RUN_TEST(test_section_open_inverted);
+    RUN_TEST(test_section_close);
 
     return UNITY_END();
 }

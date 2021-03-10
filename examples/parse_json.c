586a0a878550f96e688b3336ce63ee46b3c1657c
soasme <soasme@gmail.com>
2021-03-11 08:36:49 +1300

    add parse json example.

---
+++ examples/parse_json.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../peppapeg.h"
+#include "json.h"
+
+FILE* out = NULL;
+
+
+int main(int argc, char* argv[]) {
+    out = stdout;
+
+    P4_Grammar* grammar = P4_CreateJSONGrammar();
+    const P4_String input = "[1,2.0,3e1,true,false,null,\"xyz\",{},[]]";
+    P4_Source* source = P4_CreateSource(input, P4_JSONEntry);
+    P4_Error err = P4_Parse(grammar, source);
+    P4_Token* root = P4_GetSourceAst(source);
+    P4_JsonifySourceAst(stdout, root, P4_JSONKindToName);
+    P4_DeleteSource(source);
+    P4_DeleteGrammar(grammar);
+
+    return 0;
+}
+
+

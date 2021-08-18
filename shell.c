e5449c4ff34a7ca3054246dd46c6b3451b5c1e4e
soasme <soasme@gmail.com>
2021-08-18 21:54:14 +1200

    shell: print ast.

--- shell.c
+++ shell.c
@@ -31,16 +31,9 @@
  * @see        https://github.com/soasme/PeppaPEG
 */
 
-# ifndef P4_SHELL_H
-# define P4_SHELL_H
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <libgen.h>
 #include <peppa.h>
@@ -58,15 +51,52 @@ static void usage(const char* name) {
         "  -h\tprint help information\n"
         "  -V\tprint version\n"
         "  -g\trequired, path to peg grammar file\n"
+        "  -e\trequired, entry rule name in peg grammar\n"
     );
+}
+
+static char* read_file(FILE* f) {
+    fseek(f, 0, SEEK_END);
+    long length = ftell(f);
+    fseek (f, 0, SEEK_SET);
+    char* buf = P4_MALLOC((length+2) * sizeof(char));
+    fread(buf, sizeof(char), length, f);
+    buf[length] = '\0';
+    return buf;
+}
+
+static int print_ast(
+        const char* grammar_content,
+        const char* input_content,
+        const char* entry) {
+    int code = 0;
+    P4_Grammar* grammar = P4_LoadGrammar((char*)grammar_content);
+    P4_Source*  source = P4_CreateSource((char*)input_content, (char*)entry);
+    if (source == NULL) {
+        code = 1;
+        fprintf(stderr, "out of memory");
+        goto finalize;
+    }
+    if (P4_Parse(grammar, source) != P4_Ok) {
+        code = 1;
+        fprintf(stderr, "%s\n", P4_GetErrorMessage(source));
+        goto finalize;
+    }
+    P4_Node*    root = P4_GetSourceAst(source);
+    P4_JsonifySourceAst(stdout, root, NULL);
+finalize:
+    P4_DeleteSource(source);
+    P4_DeleteGrammar(grammar);
+    return code;
 }
 
 int main(int argc, char* argv[]) {
     int c;
     FILE* grammar_file = NULL;
-    char* grammar_content = NULL;
+    FILE* input_file = NULL;
+    char* entry = NULL;
 
-    while (read_arg(argc, argv, "Vhg:", c)) {
+    while (read_arg(argc, argv, "Vhg:e:", c)) {
         switch (c) {
             case 'V':
                 version(basename(argv[0]));
@@ -75,6 +105,9 @@ int main(int argc, char* argv[]) {
                 version(basename(argv[0]));
                 usage(basename(argv[0]));
                 exit(0);
+            case 'e':
+                entry = optarg;
+                break;
             case 'g':
                 if (!(grammar_file = fopen(optarg, "r"))) {
                     perror(optarg);
@@ -87,19 +120,45 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    argc -= optind;
+    argv += optind;
+
     if (grammar_file == NULL) {
         fprintf(stderr, "error: option -g is required\n");
         usage(basename(argv[0]));
         exit(1);
     }
 
+    if (entry == NULL) {
+        fprintf(stderr, "error: option -e is required\n");
+        usage(basename(argv[0]));
+        exit(1);
+    }
+
+    if (!argc) {
+        fprintf(stderr, "error: argument FILE is required\n");
+        usage(basename(argv[0]));
+        exit(1);
+    }
+
+    if (strcmp(*argv, "-") == 0) {
+        input_file = stdin;
+    } else {
+        if (!(input_file = fopen(*argv, "r"))) {
+            perror(*argv);
+            exit(1);
+        }
+    }
+
+    char* grammar_content = read_file(grammar_file);
+    char* input_content = read_file(input_file);
+
+    int code = print_ast(grammar_content, input_content, entry);
+
+    P4_FREE(input_content);
+    P4_FREE(grammar_content);
     fclose(grammar_file);
+    fclose(input_file);
 
-    return 0;
+    return code;
 }
-
-#ifdef __cplusplus
-}
-#endif
-
-# endif

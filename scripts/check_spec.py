8efaf202e6593e49a0988d6aca9e2df637e6538b
soasme <soasme@gmail.com>
2021-08-24 17:16:14 +1200

    go: add rune lit spec.

--- scripts/check_spec.py
+++ scripts/check_spec.py
@@ -14,7 +14,7 @@ def test_spec():
             print("invalid json spec")
             exit(1)
 
-    failed, total = 0, 0
+    failed, ignored, total = 0, 0, 0
     for spec in specs:
         for test in spec['tests']:
             total += 1
@@ -56,7 +56,7 @@ def test_spec():
                         f"GOT:\n{proc.stderr.decode('utf-8')}{proc.stdout}\n"
                     )
                     failed += 1
-            else:
+            elif 'E' in test:
                 if proc.stderr.decode('utf-8').strip() != test['E']:
                     print(
                         f"GRAMMAR:\n{spec.get('grammar') or spec.get('grammar_file')}\n"
@@ -65,8 +65,10 @@ def test_spec():
                         f"GOT:\n{proc.stderr.decode('utf-8')}{proc.stdout.decode('utf-8')}"
                     )
                     failed += 1
+            else:
+                ignored += 1
 
-    print("total: %d, failed: %d" % (total, failed))
+    print("total: %d, failed: %d, ignored: %d" % (total, failed, ignored))
     if failed:
         exit(1)
 
